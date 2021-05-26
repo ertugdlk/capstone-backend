f77f8fd2f95085c3579ed6b46e63471def1ed1ef
ertugdlk <ertgdlk@gmail.com>
2021-05-26 14:30:08 +0300

    env variables added

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import datetime
 import json
-
+import os
+from dotenv import load_dotenv
 from flask import Flask, jsonify, Response
 import psycopg2
 import repository
 import decimal
+load_dotenv()
 
 app = Flask(__name__)
-conn = psycopg2.connect(user='root', password='', port=5432,
-                        host='',
+conn = psycopg2.connect(user='root', password=os.getenv('DB_PASS'), port=5432,
+                        host=os.getenv('DB_HOST'),
                         database='capstone')
 
 @app.route('/')
